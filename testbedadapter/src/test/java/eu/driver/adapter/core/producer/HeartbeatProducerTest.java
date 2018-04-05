6377f27398c6d92186b9cb33c86772573ed939c0
TObritzh <thomas.obritzhauser@frequentis.com>
2018-04-05 11:48:20 +0200

    HeartbeatProducerTest fixed

--- testbedadapter/src/test/java/eu/driver/adapter/core/producer/HeartbeatProducerTest.java
+++ testbedadapter/src/test/java/eu/driver/adapter/core/producer/HeartbeatProducerTest.java
@@ -6,8 +6,6 @@ import org.apache.avro.generic.IndexedRecord;
 import org.apache.kafka.clients.producer.MockProducer;
 
 import eu.driver.adapter.constants.TopicConstants;
-import eu.driver.adapter.time.ISO8601TimestampProvider;
-import eu.driver.adapter.time.ITimestampProvider;
 import eu.driver.model.core.Heartbeat;
 import eu.driver.model.edxl.EDXLDistribution;
 import junit.framework.Test;
@@ -31,15 +29,14 @@ public class HeartbeatProducerTest extends TestCase {
 		MockProducer<EDXLDistribution, IndexedRecord> prod = new MockProducer<>();
 		HeartbeatProducer producer = new HeartbeatProducer(prod, TopicConstants.HEARTBEAT_TOPIC);
 		producer.startHeartbeats(50);
-		Thread.sleep(500); // for some reason it takes ~250ms before the scheduled executor becomes 'stable'
+		Thread.sleep(470); // for some reason it takes ~250ms before the scheduled executor becomes 'stable'
 		producer.stopHeartbeats();
 		
 		Heartbeat last = (Heartbeat) prod.history().get(prod.history().size()-1).value();
 		Heartbeat secondLast = (Heartbeat) prod.history().get(prod.history().size()-2).value();
 		
-		ITimestampProvider timeProvider = new ISO8601TimestampProvider();
-		Date lastDate = timeProvider.parseDate(last.getAlive().toString()).get();
-		Date secondLastDate = timeProvider.parseDate(secondLast.getAlive().toString()).get();
+		Date lastDate =  new Date(last.getAlive());
+		Date secondLastDate =  new Date(secondLast.getAlive());
 		
 		assertEquals("Time between updates should be 50 ms apart (allowed error of 5 ms)", 50, lastDate.getTime() - secondLastDate.getTime(), 5);
 		assertEquals("Should have sent 10 heartbeats in 500 ms with 50 ms update rate", 10, prod.history().size());
